8ce16b2f7d6dcf52ab81f0cf92bb6b88238a9677
natkai2040 <72627669+natkai2040@users.noreply.github.com>
2022-09-25 13:34:23 -0700

    Cleaned code

--- consec_primes.js
+++ consec_primes.js
@@ -1,7 +1,10 @@
-let num = 2; 
+
 let consec = 0;
+let targetLength = 14; // Change to any number > 0 to find nonprime sequence of this length
+let num = 2; 
 let primelist = [];
-while (consec < 14){
+
+while (consec < targetLength){ 
   if (primelist.reduce((hasdivisor, d)=> (num%d===0)? false:hasdivisor, true)){
     primelist.push(num);
     consec = 0;
@@ -11,12 +14,12 @@ while (consec < 14){
   }
   ++num;
 }
-console.log(num-1); //14th number
+let nthNonPrime = num-1; // targetLength-th number in sequence
 
 
 
 let primes = "";
-for (let i = 537; i>=523;--i){
+for (let i = nthNonPrime; i >= (nthNonPrime - targetLength);--i){ //print list of nonprimes
   primes = i.toString() + " Prime Factors: (" ;
   primelist.forEach(function(d) {
     if (i%d===0) {
